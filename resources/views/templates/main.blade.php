3d1b4be6a339f0482b12a1e9a214ee0ecd132b78
GQuantic <darkkukri0550@gmail.com>
2023-11-15 14:22:31 +0300

    Updates

---
+++ resources/views/templates/main.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Mobile Web-app fullscreen -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+
+    <!-- Meta tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="{{ asset('assets/icons/logo.png') }}">
+
+    <!--Title-->
+    <title>{{ config('app.name') }}</title>
+
+    <!--CSS styles-->
+    <link rel="stylesheet" media="all" href="css/bootstrap.css" />
+    <link rel="stylesheet" media="all" href="css/animate.css" />
+    <link rel="stylesheet" media="all" href="css/font-awesome.css" />
+    <link rel="stylesheet" media="all" href="css/furniture-icons.css" />
+    <link rel="stylesheet" media="all" href="css/linear-icons.css" />
+    <link rel="stylesheet" media="all" href="css/magnific-popup.css" />
+    <link rel="stylesheet" media="all" href="css/owl.carousel.css" />
+    <link rel="stylesheet" media="all" href="css/ion-range-slider.css" />
+    <link rel="stylesheet" media="all" href="css/theme.css" />
+
+    <!--Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600&amp;subset=latin-ext" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<div class="page-loader"></div>
+
+<div class="wrapper">
+
+    @include('blocks.navigation')
+
+    @yield('content')
+
+    @include('blocks.footer')
+
+
+</div>
+
+<!--JS files-->
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.bootstrap.js"></script>
+<script src="js/jquery.magnific-popup.js"></script>
+<script src="js/jquery.owl.carousel.js"></script>
+<script src="js/jquery.ion.rangeSlider.js"></script>
+<script src="js/jquery.isotope.pkgd.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,253 @@
+@extends('templates.main')
+
+@section('content')
+    <section class="header-content">
+
+        <div class="owl-slider">
+
+            <!-- === slide item === -->
+
+            <div class="item" style="background-image:url({{ asset('assets/gallery/gallery-1.jpg') }})">
+                <div class="box">
+                    <div class="container">
+                        <h2 class="title animated h1" data-animation="fadeInDown">Modern furniture theme</h2>
+                        <div class="animated" data-animation="fadeInUp">
+                            Modern & powerfull template. <br /> Clean design & reponsive
+                            layout. Google fonts integration
+                        </div>
+                        <div class="animated" data-animation="fadeInUp">
+                            <a href="https://themeforest.net/item/mobel-furniture-website-template/20382155" target="_blank" class="btn btn-main" ><i class="icon icon-cart"></i> Buy this template</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- === slide item === -->
+
+            <div class="item" style="background-image:url({{ asset('assets/gallery/gallery-2.jpg') }})">
+                <div class="box">
+                    <div class="container">
+                        <h2 class="title animated h1" data-animation="fadeInDown">Mobile ready!</h2>
+                        <div class="animated" data-animation="fadeInUp">Unlimited Choices. Unbeatable Prices. Free Shipping.</div>
+                        <div class="animated" data-animation="fadeInUp">Furniture category icon fonts!</div>
+                        <div class="animated" data-animation="fadeInUp">
+                            <a href="category.html" class="btn btn-clean">Get insipred</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- === slide item === -->
+
+            <div class="item" style="background-image:url({{ asset('assets/gallery/gallery-3.jpg') }})">
+                <div class="box">
+                    <div class="container">
+                        <h2 class="title animated h1" data-animation="fadeInDown">
+                            Very Animate.css Friend.
+                        </h2>
+                        <div class="desc animated" data-animation="fadeInUp">
+                            Combine with animate.css. Or just use your own!.
+                        </div>
+                        <div class="desc animated" data-animation="fadeInUp">
+                            Bunch of typography effects.
+                        </div>
+                        <div class="animated" data-animation="fadeInUp">
+                            <a href="https://themeforest.net/item/mobel-furniture-website-template/20382155" target="_blank" class="btn btn-clean">Buy this template</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div> <!--/owl-slider-->
+    </section>
+
+    <section class="owl-icons-wrapper owl-icons-frontpage">
+
+        <!-- === header === -->
+
+        <header class="hidden">
+            <h2>Product categories</h2>
+        </header>
+
+        <div class="container">
+
+            <div class="owl-icons">
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-sofa"></i>
+                        <figcaption>Sofa</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-armchair"></i>
+                        <figcaption>Armchairs</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-chair"></i>
+                        <figcaption>Chairs</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-dining-table"></i>
+                        <figcaption>Dining tables</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-media-cabinet"></i>
+                        <figcaption>Media storage</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-table"></i>
+                        <figcaption>Tables</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-bookcase"></i>
+                        <figcaption>Bookcase</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-bedroom"></i>
+                        <figcaption>Bedroom</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-nightstand"></i>
+                        <figcaption>Nightstand</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-children-room"></i>
+                        <figcaption>Children room</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-kitchen"></i>
+                        <figcaption>Kitchen</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-bathroom"></i>
+                        <figcaption>Bathroom</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-wardrobe"></i>
+                        <figcaption>Wardrobe</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-shoe-cabinet"></i>
+                        <figcaption>Shoe cabinet</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-office"></i>
+                        <figcaption>Office</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-bar-set"></i>
+                        <figcaption>Bar sets</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-lightning"></i>
+                        <figcaption>Lightning</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-carpet"></i>
+                        <figcaption>Varpet</figcaption>
+                    </figure>
+                </a>
+
+                <!-- === icon item === -->
+
+                <a href="#">
+                    <figure>
+                        <i class="f-icon f-icon-accessories"></i>
+                        <figcaption>Accessories</figcaption>
+                    </figure>
+                </a>
+
+            </div> <!--/owl-icons-->
+        </div> <!--/container-->
+    </section>
+
+
+    @include('blocks.product-categories')
+@endsection
